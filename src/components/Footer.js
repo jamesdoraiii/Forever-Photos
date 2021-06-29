a34dc35124e116901b1c7f6ce3515301d6964552
James Dora <jamesdoraiii@gmail.com>
2021-06-29 16:05:54 -0500

    added pricing to the nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/foreverphotos/">@foreverphotos</a>
     </h2>
     <br />
     <footer className="footer">
